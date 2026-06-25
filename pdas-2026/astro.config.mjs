2ad8feb8329b4b48513ec6262209404a002a412f
learnomail <learnomail@gmail.com>
2026-06-25 15:14:04 +0530

    astro_change

--- pdas-2026/astro.config.mjs
+++ pdas-2026/astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Add these two lines below
+  site: 'https://learnomail.github.io',
+  base: '/pdas-2026', 
+
   vite: {
     plugins: [tailwindcss()]
   }
