43344a7f74f007798d79c259574aefb415bda48d
learnomail <learnomail@gmail.com>
2026-06-25 16:11:36 +0530

    changes_for_hosting

--- pdas-2026/src/config/navigation.js
+++ pdas-2026/src/config/navigation.js
@@ -1,34 +1,36 @@
 // src/config/navigation.js
+const base = import.meta.env.BASE_URL.replace(/\/$/, '');
+
 export const navLinks = [
-  { name: 'HOME', href: '/' },
+  { name: 'HOME', href: `${base}/` },
   { 
     name: 'ABOUT PDAS 2026+', 
     href: '#',
     dropdown: [
-      { name: 'Exhibitor Profile', href: '/exhibitor-profile' },
-      { name: 'Visitor Profile', href: '/visitor-profile' },
-      { name: 'Who Should Participate', href: '/who-should-participate' },
-      { name: 'Why To Participate', href: '/why-to-participate' },
-      { name: 'Participating Options', href: '/participating-options' }
+      { name: 'Exhibitor Profile', href: `${base}/exhibitor-profile` },
+      { name: 'Visitor Profile', href: `${base}/visitor-profile` },
+      { name: 'Who Should Participate', href: `${base}/who-should-participate` },
+      { name: 'Why To Participate', href: `${base}/why-to-participate` },
+      { name: 'Participating Options', href: `${base}/participating-options` }
     ]
   },
   { 
     name: 'KEY HIGHLIGHTS+', 
     href: '#',
     dropdown: [
-      { name: 'Expo', href: '/expo' },
-      { name: 'B2B & B2G Meetings', href: '/brb-b2g' },
-      { name: 'Strategic Panel Discussion', href: '/spd' }
+      { name: 'Expo', href: `${base}/expo` },
+      { name: 'B2B & B2G Meetings', href: `${base}/brb-b2g` },
+      { name: 'Strategic Panel Discussion', href: `${base}/spd` }
     ]
   },
-  { name: 'SUPPORTED BY', href: '/supported-by' },
+  { name: 'SUPPORTED BY', href: `${base}/supported-by` },
   { 
     name: 'ORGANISERS+', 
     href: '#',
     dropdown: [
-      { name: 'About TPF', href: '/about-tpf' },
-      { name: 'PDAS Organisation', href: '/pdas-organization' }
+      { name: 'About TPF', href: `${base}/about-tpf` },
+      { name: 'PDAS Organisation', href: `${base}/pdas-organization` }
     ]
   },
-  { name: 'CONTACT US', href: '/contact' }
+  { name: 'CONTACT US', href: `${base}/contact` }
 ];
